b51de9fceba0c9741fc10653e9991253ec57a582
KeeHanXiang <khx07602@gmail.com>
2024-07-22 21:10:04 +0800

    Update AppTest.java

--- jenkins-php-selenium-test/src/test/java/mycompany/app/AppTest.java
+++ jenkins-php-selenium-test/src/test/java/mycompany/app/AppTest.java
@@ -23,7 +23,7 @@ public class AppTest
 {
 	WebDriver driver; 
 	WebDriverWait wait; 
-	String url = "http://localhost";
+	String url = "http://172.29.112.1:8080";
 	String validEmail = "user@example.com";
 	String validPassword = "password1234";
 	String invalidEmail = "none@example.com";
